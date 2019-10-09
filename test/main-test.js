e06c7f8e97af0daaac3bf2b0f8b1da21bf96975b
KATRINA CHLOE REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESKA@OOCL.COM>
2019-10-09 22:23:24 +0800

    Created Test Cases for POS

--- test/main-test.js
+++ test/main-test.js
@@ -1,6 +1,138 @@
 'use strict';
 
 describe('pos', () => {
+  it('should decode barcodes', () => {
+    const decodedBarcodes = {
+      bacode: 'ITEM000001'
+    };
+
+    let expected = loadItems(decodedBarcodes);
+    expect(expected).toEqual([{"barcode": "ITEM000001", "count": 1}]);
+  });
+
+  it('should load items with barcodes', () => {
+    const decodedBarcodes = {
+      bacode: 'ITEM000001'
+    };
+
+    let expected = loadItems(decodedBarcodes);
+    expect(expected).toEqual([{"barcode": "ITEM000001", "name": "Diet Coke", "price": 3.00 , "unit": 'bottle'}]);
+  });
+
+  it('should combine items', () => {
+    const decodedBarcodes = {
+      bacode: 'ITEM000001'
+    };
+
+    let expected = loadItems(decodedBarcodes);
+    expect(expected).toEqual([{"barcode": "ITEM000001", "name": "Diet Coke", "price": 3.00 , "unit": 'bottle', "count": 1}]);
+  });
+
+  it('should decode tags', () => {
+    const decodedBarcodes = {
+      bacode: 'ITEM000001'
+    };
+
+    let expected = decodeTags(decodedBarcodes);
+    expect(expected).toEqual([{"barcode": "ITEM000001", "name": "Diet Coke", "price": 3.00 , "unit": 'bottle', "count": 1}]);
+  });
+
+  
+  it('should promote receipt tags', () => {
+    const items = [{
+      "barcode": "ITEM000001",
+      "name": "Diet Coke",
+      "price": 3.00 ,
+      "unit": 'bottle',
+      "count": 1},
+      {"barcode": "ITEM000001",
+        "name": "Diet Coke",
+        "price": 3.00 ,
+        "unit": 'bottle',
+        "count": 1},
+      {"barcode": "ITEM000001",
+        "name": "Diet Coke",
+        "price": 3.00 ,
+        "unit": 'bottle',
+        "count": 1}];
+
+      const promotions = ['ITEM000001'];
+
+    let expected = promotedReceiptItems(items,promotions);
+    expect(expected).toEqual([
+      {"barcode": "ITEM000001",
+      "name": "Diet Coke",
+      "price": 3.00 ,
+      "unit": 'bottle',
+      "count": 1,
+      "Subtotal": 6.00+"(yuan)"}]);
+  });
+
+  it('should calculate receipt items', () => {
+    const items = [{
+      "barcode": "ITEM000001",
+      "name": "Diet Coke",
+      "price": 3.00 ,
+      "unit": 'bottle',
+      "count": 1},
+      {"barcode": "ITEM000001",
+        "name": "Diet Coke",
+        "price": 3.00 ,
+        "unit": 'bottle',
+        "count": 1},
+      {"barcode": "ITEM000001",
+        "name": "Diet Coke",
+        "price": 3.00 ,
+        "unit": 'bottle',
+        "count": 1}];
+
+    let expected = calculateReceiptItems(items);
+    expect(expected).toEqual([
+    {"barcode": "ITEM000001",
+    "name": "Diet Coke",
+    "price": 3.00 ,
+    "unit": 'bottle',
+    "count": 3,
+    "Subtotal": 6.00+"(yuan)"}]);
+  });
+
+  it('should calculate receipt total', () => {
+    const receipItems = [
+      {"barcode": "ITEM000001",
+      "name": "Diet Coke",
+      "price": 3.00 ,
+      "unit": 'bottle',
+      "count": 3,
+      "Subtotal": 6.00+"(yuan)"}];
+
+    let expected = calculateReceiptTotal(receipItems);
+    expect(expected).toEqual({"Total":6.00+"(yuan)"});
+  });
+  
+  it('should calculate receipt savings', () => {
+    const receipItems = [
+      {"barcode": "ITEM000001",
+      "name": "Diet Coke",
+      "price": 3.00 ,
+      "unit": 'bottle',
+      "count": 3,
+      "Subtotal": 6.00+"(yuan)"}];
+
+    let expected = calculateReceiptSavings(receipItems);
+    expect(expected).toEqual({"Savings":3.00+"(yuan)"});
+  });
+    
+  it('should render receipt', () => {
+    const receipt = "Name：Coca-Cola，Quantity：3 bottles，Unit：3.00(yuan)，Subtotal：6.00(yuan)";
+      receipt
+    let expected = calculateReceiptSavings(receipItems);
+    expect(expected).toEqual("***<store earning no money>Receipt ***"+
+    "Name：Coca-Cola，Quantity：3 bottles，Unit：3.00(yuan)，Subtotal：6.00(yuan)"+
+    "----------------------"+
+    "Total：6(yuan)"+
+    "Discounted prices：3(yuan)"+
+    "**********************`");
+  });
 
   it('should print text', () => {
 
